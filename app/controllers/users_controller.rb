b21feb4b2d0262a5b7483229c9ef81031cfe4b86
Steven Yi <stevenyi@gmail.com>
2014-02-24 15:24:17 -0500

    finished going through simple password authentication for Sorcery (https://github.com/NoamB/sorcery/wiki/Simple-Password-Authentication)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+  skip_before_filter :require_login, only: [:index, :new, :create]
+
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
@@ -28,7 +31,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html { redirect_to :users, notice: 'User was successfully created.' }
         format.json { render action: 'show', status: :created, location: @user }
       else
         format.html { render action: 'new' }
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 CsoundNotebook::Application.routes.draw do
+ # get "user_sessions/new"
+ # get "user_sessions/create"
+ # get "user_sessions/destroy"
+
+  root :to => 'users#index'
+  resources :user_sessions
   resources :users
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
