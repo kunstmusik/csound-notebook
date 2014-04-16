1961b228e805c4cace0a6b8e03b26c9f892f0a95
Steven Yi <stevenyi@gmail.com>
2014-04-16 09:56:25 -0400

    changed redirect to login_path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to :users, notice: 'Account created successfully. Please check your email to activate your account before logging in.' }
+        format.html { redirect_to login_path, notice: 'Account created successfully. Please check your email to activate your account before logging in.' }
         format.json { render action: 'show', status: :created, location: @user }
       else
         format.html { render action: 'new' }
