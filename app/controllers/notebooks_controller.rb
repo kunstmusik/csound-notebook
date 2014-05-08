d7b3c0c232f8c91ab31febd1b1228cca93fc405d
Steven Yi <stevenyi@gmail.com>
2014-05-08 12:51:40 +0100

    protect against deleting other user's notes and notebooks

--- app/controllers/notebooks_controller.rb
+++ app/controllers/notebooks_controller.rb
@@ -70,7 +70,8 @@ class NotebooksController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_notebook
-      @notebook = Notebook.find(params[:id])
+      n =  Notebook.find(params[:id])
+      @notebook = (n.user_id == @current_user.id ? n : nil)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -75,7 +75,8 @@ class NotesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_note
-      @note = Note.find(params[:id])
+      n =  Note.find(params[:id])
+      @note = (n.user_id == @current_user.id ? n : nil)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
