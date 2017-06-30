573005493f9c0c1a0524e7a3eef7ef5a8dbe759d
Steven Yi <stevenyi@gmail.com>
2017-06-30 19:22:29 -0400

    add notes table db migration, working on structuring site for notes save/load functionality

--- src/clj/csound_notebook/routes/note.clj
+++ src/clj/csound_notebook/routes/note.clj
@@ -1,13 +1,36 @@
 (ns csound-notebook.routes.note
   (:require [csound-notebook.layout :as layout]
+            [csound-notebook.db.core :as db]
             [compojure.core :refer [defroutes GET POST context]]
             [ring.util.http-response :as response]
             [clojure.java.io :as io]))
 
+(def valid-chars
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 
+(def num-chars (.length valid-chars))
 
-(defn note-page [req]
+(defn gen-note-id
+  "Generates string with 8 random alphanumeric characters."
+  []  
+  (str
+    (.charAt valid-chars (int (* num-chars (Math/random))))
+    (.charAt valid-chars (int (* num-chars (Math/random))))
+    (.charAt valid-chars (int (* num-chars (Math/random))))
+    (.charAt valid-chars (int (* num-chars (Math/random))))
+    (.charAt valid-chars (int (* num-chars (Math/random))))
+    (.charAt valid-chars (int (* num-chars (Math/random))))
+    (.charAt valid-chars (int (* num-chars (Math/random))))
+    (.charAt valid-chars (int (* num-chars (Math/random))))
+    ))
+
+(defn note-page 
+  [req]
   (layout/render "note.html"))
+
+(defn save-note 
+  [req]
+  {:body {:note_id "12345678"}})
 
 
 (defroutes note-routes
@@ -15,8 +38,16 @@
     "/note" []
 
     (GET "/" req 
-         (note-page req))
+         (layout/render "note.html"))
     (GET "/:id" req 
-         (note-page req))
+         (layout/render "note.html"))
+    (GET "/:username/:id" req 
+         (layout/render "note.html"))
+    (POST "/" req
+          (save-note req))
+    (POST "/:id" req
+          (save-note req))
+    (POST "/:username/:id" req
+          (save-note req))
     ))
   
